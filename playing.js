5770805987c8f5ec876ee67711942d060f3367dd
nika09-and <andriadze.n1@gmail.com>
2025-02-27 17:37:57 +0400

    Update playing.js

--- playing.js
+++ playing.js
@@ -1,3 +1,5 @@
+import "./libs/DragDropTouch.js";
+
 document.addEventListener('DOMContentLoaded', () => {
     const cardsContainer = document.getElementById('cards');
     const shuffleButton = document.getElementById('shuffleButton');
